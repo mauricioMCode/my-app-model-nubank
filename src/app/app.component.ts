8b33bf3e1a3cd3fc3fc43dadb45f811daf3d0830
Mauricio Pantoja <mcpantoja@outlook.com>
2022-09-04 22:38:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    '../../node_modules/keen-slider/keen-slider.min.css'
+  ]
 })
 export class AppComponent {
   title = 'my-app-model-nubank';
+
+  @ViewChild('sliderRef')
+  sliderRef!: ElementRef<HTMLElement>
+  @ViewChild('cardSliderRef')
+  cardSliderRef!: ElementRef<HTMLElement>
+
+  slider?: KeenSliderInstance 
+  cardSlider?: KeenSliderInstance 
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: false,
+      mode: 'free',
+      slides: {
+        perView: 4.5,
+        spacing: 14
+      }
+    });
+    this.cardSlider = new KeenSlider(this.cardSliderRef.nativeElement, {
+      loop: false,
+      mode: 'free',
+      slides: {
+        perView: 1.5,
+        spacing: 14
+      }
+    });
+  }
 }
